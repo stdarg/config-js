7f19495f56561a95558ddabcaa14366b86ccc4a3
Edmond Meinfelder <edmond@stdarg.com>
2013-02-24 18:31:47 -0800

    Added a simple cfg example with multiple languages.

---
+++ test/cfg_example2.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+
+    en: {
+        welcome: "Welcome to this file."
+    },
+
+    de: {
+        welcome: "Willkommen zu dieser datei."
+    },
+
+    es: {
+        welcome: "Bienvenidos a este archivo."
+    }
+};
