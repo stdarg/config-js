30bb14750fe8535e789be3ae37f37dedcc4afdfc
Edmond Meinfelder <edmond@stdarg.com>
2013-02-24 18:27:51 -0800

    Flushed out tests with real test cases.

--- test/test.js
+++ test/test.js
@@ -20,9 +20,28 @@ describe('Config()', function() {
     });
 });
 
-describe('Config()', function() {
-    it('Should not throw if pathToConfigFile does point to a file.', function() {
-        new Config('./test/cfg_example.js');
+describe('get()', function() {
+    it('Should retrieve values from the configuration file.', function() {
+        var config = new Config('./test/cfg_example.js');
+        assert.ok(config.get('server.port') === 4201);
+        assert.ok(config.get('logging.name') === 'mush.js');
+        // test default value
+        assert.ok(config.get('No.Such.Value.Exists', 511) === 511);
     });
 });
 
+describe('getByRegion()', function() {
+    it('Should retrieve values from the configured region of the config file.', function() {
+        var config = new Config('./test/cfg_example2.js');
+        assert.ok(config.getByRegion('welcome') === 'Welcome to this file.');
+        assert.ok(config.get('en.welcome') === 'Welcome to this file.');
+
+        var config = new Config('./test/cfg_example2.js', 'de');
+        assert.ok(config.getByRegion('welcome') === 'Willkommen zu dieser datei.');
+        assert.ok(config.get('de.welcome') === 'Willkommen zu dieser datei.');
+
+        var config = new Config('./test/cfg_example2.js', 'es');
+        assert.ok(config.getByRegion('welcome') === 'Bienvenidos a este archivo.');
+        assert.ok(config.get('es.welcome') === 'Bienvenidos a este archivo.');
+    });
+});
