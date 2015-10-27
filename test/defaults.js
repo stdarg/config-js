21cb4767597426de6169a79e08730460d09dd54f
Edmond Meinfelder <edmond@stdarg.com>
2015-10-27 10:18:28 -0700

    Adding defaults.

---
+++ test/defaults.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+
+    mush : {
+        version : '0.0.1',
+        name : 'mush2.js',
+        variant : '',
+        defaultVal: 'Hi default'
+    },
+
+    defaultTest : {
+        working : true
+    },
+};
